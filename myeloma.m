a6801444a452acbb9694a16b61dd6f9e87439885
cgomezmolina <144175901+cgomezmolina@users.noreply.github.com>
2023-11-29 06:28:51 +0100

    Update myeloma.m

--- myeloma.m
+++ myeloma.m
@@ -29,30 +29,33 @@ BVL_high = abs(-2*y1_high + 0.334*y2_high);
 
 % For second highest proliferation rate of the cancer:
 %OBa rate
-y1_2high = 
+y1_2high = 7.954*10.^-10*t.^3 - 2.499*10.^-6*t.^2 + 0.002571*t + 0.7264
+
 
 %OCa rate
-y2_2high = 
+y2_2high  = 2.171*10.^-10*t.^3 - 8.107*10.^-7*t.^2 + 0.001279*t + 1.133
+
+ 
 
 % We compute the bone volume percentage loss due to mm:
 BVL_2high = abs(-2*y1_2high + 0.334*y2_2high);
 
 % For the second lowest proliferation rate of the cancer:
 %OBa rate
-y1_2low = 
+y1_2low = 7.111*10.^-10*t.^3 - 2.151*10.^-6*t.^2 + 0.002192*t + 0.671
 
 %OCa rate
-y2_2low = 
+y2_2low = 3.301*10.^-10*t.^3 - 9.891*10.^-7*t.^2 + 0.001193*t + 1.111
 
 % We compute the bone volume percentage loss due to mm:
 BVL_2low = abs(-2*y1_2low + 0.334*y2_2low);
 
 % For the  lowest proliferation rate of the cancer:
 %OBa rate
-y1_low = 
+y1_low = 5.381*10.^-10*x.^3 - 1.543*10.^-6*x.^2 +0.001343*x +0.9461
 
 %OCa rate
-y2_low = 
+y2_low = 2.083*10.^-10*x.^3 - 6.61*10.^-7*x.^2 + 0.0007185*x + 1.119
 
 % We compute the bone volume percentage loss due to mm:
 BVL_low = abs(-2*y1_low + 0.334*y2_low);
