aefb7ad847045e224179cedefd09138636097183
rjackson0387 <103905028+rjackson0387@users.noreply.github.com>
2023-11-29 08:34:25 -0500

    Update myeloma.m

--- myeloma.m
+++ myeloma.m
@@ -29,36 +29,39 @@ BVL_high = abs(-2*y1_high + 0.334*y2_high);
 
 % For second highest proliferation rate of the cancer:
 %OBa rate
-y1_2high = 7.954*10.^-10*t.^3 - 2.499*10.^-6*t.^2 + 0.002571*t + 0.7264
+y1_2high = 7.954*10.^-10*t.^3 - 2.499*10.^-6*t.^2 + 0.002571*t + 0.7264;
 
 %OCa rate
-y2_2high  = 2.171*10.^-10*t.^3 - 8.107*10.^-7*t.^2 + 0.001279*t + 1.133
+y2_2high  = 2.171*10.^-10*t.^3 - 8.107*10.^-7*t.^2 + 0.001279*t + 1.133;
 
 % We compute the bone volume percentage loss due to mm:
 BVL_2high = abs(-2*y1_2high + 0.334*y2_2high);
 
 % For the second lowest proliferation rate of the cancer:
 %OBa rate
-y1_2low = 7.111*10.^-10*t.^3 - 2.151*10.^-6*t.^2 + 0.002192*t + 0.671
+y1_2low = 7.111*10.^-10*t.^3 - 2.151*10.^-6*t.^2 + 0.002192*t + 0.671;
 
 %OCa rate
-y2_2low = 3.301*10.^-10*t.^3 - 9.891*10.^-7*t.^2 + 0.001193*t + 1.111
+y2_2low = 3.301*10.^-10*t.^3 - 9.891*10.^-7*t.^2 + 0.001193*t + 1.111;
 
 % We compute the bone volume percentage loss due to mm:
 BVL_2low = abs(-2*y1_2low + 0.334*y2_2low);
 
 % For the  lowest proliferation rate of the cancer:
 %OBa rate
-y1_low = 5.381*10.^-10*x.^3 - 1.543*10.^-6*x.^2 +0.001343*x +0.9461
+y1_low = 5.381*10.^-10*t.^3 - 1.543*10.^-6*t.^2 +0.001343*t +0.9461;
 
 %OCa rate
-y2_low = 2.083*10.^-10*x.^3 - 6.61*10.^-7*x.^2 + 0.0007185*x + 1.119
+y2_low = 2.083*10.^-10*t.^3 - 6.61*10.^-7*t.^2 + 0.0007185*t + 1.119;
 
 % We compute the bone volume percentage loss due to mm:
 BVL_low = abs(-2*y1_low + 0.334*y2_low);
 
 % integration of BMD loss with BV loss due to multiple myeloma:
-TotalBMD = (((-69) * exp(lambda * t .* BVL_high) + 69) * -1 );
+TotalBMD_high = (((-69) * exp(lambda * t .* BVL_high) + 69) * -1 );
+TotalBMD_2high = (((-69) * exp(lambda * t .* BVL_2high) + 69) * -1 );
+TotalBMD_2low = (((-69) * exp(lambda * t .* BVL_2low) + 69) * -1 );
+TotalBMD_low = (((-69) * exp(lambda * t .* BVL_low) + 69) * -1 );
 
 % Plot for intial bone mineral density loss
 figure
@@ -66,11 +69,24 @@ plot(t, BMD, 'r', 'LineWidth', 1.5)
 grid on
 hold on
 
-% Addition of vertical line to indicate length of round trip mars mission
-xline(1095, 'b')
-text(1095, max(BMD), 'Day 1095', 'VerticalAlignment', 'top', 'HorizontalAlignment', 'left', 'Color', 'b');
+% Plots for bmd loss with MM
+plot(t,TotalBMD_low, 'k', 'LineWidth', 1.5);
+hold on
+plot(t,TotalBMD_2low, 'm', 'LineWidth', 1.5);
+hold on
+plot(t,TotalBMD_2high, 'g', 'LineWidth', 1.5);
+hold on
+plot(t,TotalBMD_high, 'b', 'LineWidth', 1.5);
+hold on
 
-plot(t,TotalBMD);
-title('BMD% change with both conditions integrated')
+% Addition of vertical line to indicate length of round trip mars mission
+xline(1095, '--')
+text(1095, max(BMD), 'Day 1095', 'VerticalAlignment', 'top', 'HorizontalAlignment', 'left');
+
+legend('Pmm = 0.00/day', 'Pmm = 0.035/day', 'Pmm = 0.045/day', 'Pmm = 0.055/day', 'Pmm = 0.065/day')
+lgd = legend;
+lgd.FontSize = 12;
+lgd.Title.String = 'Proliferation Rates';
+title('BMD% Change with Microgravity and Multiple Myeloma Proliferation Rates')
 xlabel("Time (days)")
 ylabel("Bone Mineral Density Change (%)")
